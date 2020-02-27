8203e263893311b958321e71ad06c4cc606cd778
Vladimir Samorodov <vovchikan@gmail.com>
2020-02-28 00:21:48 +0500

    Added 2 hash functions
    * New file - HashFunc.cs with static class - HashFunc
    * Computed hashes of files in Program.cs and saved it
      in queue, then printed them in console.

---
+++ HashFunc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace HashFiles
+{
+    static class HashFunc
+    {
+        public static string ComputeMD5Checksum(string path)
+        {
+            using (FileStream fs = System.IO.File.OpenRead(path))
+            {
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] fileData = new byte[fs.Length];
+                fs.Read(fileData, 0, (int)fs.Length);
+                byte[] checkSum = md5.ComputeHash(fileData);
+                string result = BitConverter.ToString(checkSum).Replace("-", String.Empty);
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Хэш-сумма для больших файлов
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string ComputeMD5Checksum2(string path)
+        {
+            using (FileStream fs = System.IO.File.OpenRead(path))
+            using (MD5 md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] checkSum = md5.ComputeHash(fs);
+                string result = BitConverter.ToString(checkSum).Replace("-", String.Empty);
+                return result;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,10 @@ namespace HashFiles
 {
     class Program
     {
+        private static Queue<string> hashSums = new Queue<string>();
+        private delegate string MyHashFunc(string fileName);
+        private static bool search = true;
+
         static void Main(string[] args)
         {
             if(args.Length == 0)
@@ -17,10 +21,70 @@ namespace HashFiles
             else
             {
                 // Начать поиска файлов
-                Task.Run(() => FindFiles.GetFiles(args));
+                Task find = Task.Run(() =>
+                {
+                    try
+                    {
+                        FindFiles.GetFiles(args);
+                    }
+                    finally
+                    {
+                        search = false;
+                    }
+                });
+                Task.Run(() =>
+                {
+                    try
+                    {
+                        ComputeHashSums(HashFunc.ComputeMD5Checksum2, FindFiles.files);
+                    }
+                    finally
+                    {
+                        ;
+                    }
+                });
             }
 
             Console.ReadKey();
+            lock (hashSums)
+            {
+                while(hashSums.Count > 0)
+                {
+                    Console.WriteLine(hashSums.Dequeue());
+                }
+            }
+            Console.ReadKey();
+        }
+
+        private static void ComputeHashSums(MyHashFunc hf, Queue<string> queue)
+        {
+            string fileName;
+            int qc = 0;
+            while (search || qc > 0) {
+                try
+                {
+                    lock (queue)
+                    {
+                        fileName = queue.Dequeue();
+                        qc = queue.Count;
+                    }
+                    string res = hf(fileName);
+                    AddHashSum(res, fileName);
+                }
+                catch (InvalidOperationException)
+                {
+                    Task.Delay(300);
+                }
+            }
+            
+        }
+
+        private static void AddHashSum(string hashSum, string fileName)
+        {
+            lock (hashSums)
+            {
+                hashSums.Enqueue(string.Join(" ", fileName, hashSum));
+            }
         }
     }
 }
