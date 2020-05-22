8e00bcf0c95ec11704355e73940d6cf1156e3247
Vladimir Samorodov <vovchikan@gmail.com>
2020-05-22 15:43:37 +0500

    Added unit tests

---
+++ tests/TestDataBaseWorker/TestSQLConnection.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDataBaseWorker
+{
+    [TestFixture]
+    public class TestSQLConnection
+    {
+        [Test]
+        public void LocalSqlServerConnection()
+        {
+            using (var sc = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB;"))
+            {
+                sc.Open();
+                Assert.AreEqual(sc.State, System.Data.ConnectionState.Open);
+            }
+        }
+
+        [Test]
+        public void FindDatabase1AtLocalSqlServer(
+            [Values(defaultConnectionString, relativeConnectionString)] String connectionString)
+        {
+            using (var sc = new SqlConnection(connectionString))
+            {
+                
+                sc.Open();
+                Assert.AreEqual(sc.State, System.Data.ConnectionState.Open);
+            }
+        }
+
+        private const String defaultConnectionString = @"Data Source = (localdb)\MSSQLLocalDB;
+                AttachDbFilename=D:\Programming\C#\Github-portfolio\HashFiles\Database1.mdf;
+                Integrated Security=True;Connect Timeout=30;database=Database1";
+
+        private const String relativeConnectionString = @"Data Source = (localdb)\MSSQLLocalDB;
+                AttachDbFilename=|DataDirectory|\Database1.mdf;
+                Integrated Security=True;Connect Timeout=30;";
+
+    }
+}
---
+++ tests/TestRecursiveFilesCollector/WrongInputTesting.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using HashFiles;
+using System.IO;
+using System;
+
+namespace TestRecursiveFilesCollector
+{
+    [TestFixture]
+    public class WrongInputTesting
+    {
+
+        [OneTimeSetUp]
+        public void CreateTempDirsAndFiles()
+        {
+        }
+
+        [Test]
+        public void GetExceptionWhenArgIsEmptyString()
+        {
+            Assert.Throws<ArgumentException>(() => RecursiveFilesCollector.GetFileQueue(""));
+        }
+
+        [OneTimeTearDown]
+        public void DeleteTempDirsAndFile()
+        {
+        }
+    }
+}
