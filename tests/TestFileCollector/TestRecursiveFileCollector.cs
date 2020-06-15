aab866b23cea51c31802e96d92ddaea582d1ee2d
Vladimir Samorodov <vovchikan@gmail.com>
2020-06-15 19:49:52 +0500

    Modified test classes, project
    * Combine testing of collecting files in one test project
    * Combine test classes in 1
    * Fix globalvars, tempdirpath now dynamcly initializated

---
+++ tests/TestFileCollector/TestRecursiveFileCollector.cs
@@ -0,0 +1,47 @@
+﻿using HashFiles;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace TestCollectingFiles
+{
+    [TestFixture]
+    public class TestRecursiveFileCollector
+    {
+        private static RecursiveFileCollector recursiveCollector;
+        private static MyConcurrentQueue<string> stash;
+
+        [OneTimeSetUp]
+        public void InitizalizationFields()
+        {
+            GlobalVars.InitTempDir();
+            recursiveCollector = new RecursiveFileCollector();
+        }
+
+        [SetUp]
+        public void UpdateStaticFields()
+        {
+            stash = new MyConcurrentQueue<string>();
+            recursiveCollector.SetStash(stash);
+        }
+
+        [Test]
+        public void TempFilesCount()
+        {
+            recursiveCollector.CollectFrom(GlobalVars.tempDirPath);
+            Assert.AreEqual(GlobalVars.tempFilesCount, stash.Count);
+        }
+
+        [Test]
+        public void GetExceptionWhenArgIsEmptyString()
+        {
+            Assert.Throws<ArgumentException>(() => recursiveCollector.CollectFrom(""));
+        }
+
+        [Test]
+        public void GetExceptionWhenFakeFileInput()
+        {
+            Assert.Throws<ArgumentException>(() => recursiveCollector.CollectFrom("fake-file.txt"));
+        }
+    }
+}
---
+++ tests/TestFileCollector/TestThreadFileCollector.cs
@@ -0,0 +1,28 @@
+﻿using HashFiles;
+using NUnit.Framework;
+
+namespace TestCollectingFiles
+{
+    [TestFixture]
+    public class TestThreadFileCollector
+    {
+        private static MyConcurrentQueue<string> stash;
+        private static ThreadFileCollector dirCollector;
+
+        [SetUp]
+        public void InitializationStaticFields()
+        {
+            GlobalVars.InitTempDir();
+            stash = new MyConcurrentQueue<string>();
+            dirCollector = new ThreadFileCollector(GlobalVars.tempDirPath);
+        }
+
+        [Test]
+        public void CountTempFilesWhenTempDir()
+        {
+            dirCollector.CollectFilesToStash(stash);
+            dirCollector.Join();
+            Assert.AreEqual(GlobalVars.tempFilesCount, stash.Count);
+        }
+    }
+}
