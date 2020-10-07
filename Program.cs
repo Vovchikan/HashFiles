efd20af63d759c77ac2032a6660fd24d4d56173e
Vladimir Samorodov <vovchikan@gmail.com>
2020-10-07 12:55:24 +0500

    Realesed of catching exceptions

--- Program.cs
+++ Program.cs
@@ -32,59 +32,66 @@ namespace HashFiles
                         search = false;
                     }
                 });
-                Task.Run(() =>
+
+                // Рассчитать хэш-суммы для файлов
+                Task compute = Task.Run(() =>
                 {
-                    try
+                    int qc = 0;
+                    while (search || qc > 0)
                     {
-                        ComputeHashSums(HashFunc.ComputeMD5Checksum2, FindFiles.files);
-                    }
-                    finally
-                    {
-                        ;
+                        try
+                        {
+                            var res = FindFiles.TakeFromStorage();
+                            qc = res.Item2;
+                            computeHashSums(HashFunc.ComputeMD5Checksum, res.Item1);
+                        }
+                        catch (InvalidOperationException)
+                        {
+                            //Если хранилище пустое - ждать
+                            Task.Delay(300);
+                        }
                     }
                 });
+
+                // Добавить файлы в бд
+
+                Task.WaitAll(find, compute);
             }
 
+            Console.WriteLine("Работа закончена.");
             Console.ReadKey();
-            lock (hashSums)
-            {
-                while(hashSums.Count > 0)
-                {
-                    Console.WriteLine(hashSums.Dequeue());
-                }
-            }
-            Console.ReadKey();
+
         }
 
-        private static void ComputeHashSums(MyHashFunc hf, Queue<string> queue)
+        private static void computeHashSums(MyHashFunc hf, string fileName)
         {
-            string fileName;
-            int qc = 0;
-            while (search || qc > 0) {
-                try
-                {
-                    lock (queue)
-                    {
-                        fileName = queue.Dequeue();
-                        qc = queue.Count;
-                    }
-                    string res = hf(fileName);
-                    AddHashSum(res, fileName);
-                }
-                catch (InvalidOperationException)
-                {
-                    Task.Delay(300);
-                }
+            string  hashSum ="", errors="";
+            try
+            {
+                hashSum = hf(fileName);
             }
-            
+            catch (HashFunc.HashFuncException ex)
+            {
+                // Поймать исключение у хэш-функции
+                errors = ex.Message;
+            }
+            finally
+            {
+                errors = string.IsNullOrEmpty(errors) ? "No errors." : errors;
+                hashSum = string.IsNullOrEmpty(errors) ? "No hashsum" : hashSum;
+                addHashSum(hashSum, fileName, errors);
+            }
         }
 
-        private static void AddHashSum(string hashSum, string fileName)
+        private static void addHashSum(string hashSum, string fileName, string errors="", string sep=" ")
         {
             lock (hashSums)
             {
-                hashSums.Enqueue(string.Join(" ", fileName, hashSum));
+                var res = string.Join(sep, fileName, hashSum, errors);
+                hashSums.Enqueue(res);
+                Console.WriteLine(res);
             }
         }
+
     }
 }
