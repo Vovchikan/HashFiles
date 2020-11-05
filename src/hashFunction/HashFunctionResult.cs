f75db211f512d02416de44fd246151fc1d9605cf
Vladimir Samorodov <vovchikan@gmail.com>
2020-11-05 14:46:31 +0500

    Added method ToStringShort()

--- src/hashFunction/HashFunctionResult.cs
+++ src/hashFunction/HashFunctionResult.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,25 @@ namespace HashFiles
         public override string ToString()
         {
             return String.Format("файл: {0}, хэш-сумма:{1}, наличие ошибок:{2}", filePath, hashSum, error.Message);
+        }
+
+        public string ToStringShort()
+        {
+            string shortHash = GetShortHashSumOrEmptyString();
+            FileInfo fi = new FileInfo(filePath);
+            return $"{fi.Name} -> {shortHash}... , {error.Message}";
+        }
+
+        private string GetShortHashSumOrEmptyString()
+        {
+            int hashElementsCount = 6;
+            string result = "";
+            try
+            {
+                result = hashSum.Substring(0, hashElementsCount);
+            }
+            catch { }
+            return result;
         }
     }
 }
